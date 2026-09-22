206d06a19eaa5fe3550a44ac9bc4cd5eb971735a
mnvcodes11 <manavkumbhar4@gmail.com>
2026-09-22 16:52:45 +0530

    Fix worker portal runtime error

--- client/src/pages/WorkerLogin.jsx
+++ client/src/pages/WorkerLogin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import { useAuth } from '../context/AuthContext';
 import Navbar from '../components/Navbar';
