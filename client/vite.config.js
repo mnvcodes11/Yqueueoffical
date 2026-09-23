a42bbc32cac17811b95203ed8c13f8361221ba6f
mnvcodes11 <manavkumbhar4@gmail.com>
2026-09-23 10:09:09 +0530

    Fix production frontend runtime error

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'remove-framer-motion-browser-require',
+      generateBundle(_options, bundle) {
+        const expression = 'require("@emotion/is-prop-valid").default'
+        Object.values(bundle).forEach((output) => {
+          if (output.type === 'chunk' && output.code.includes(expression)) {
+            output.code = output.code.replaceAll(expression, 'undefined')
+          }
+        })
+      },
+    },
+  ],
   server: {
     host: true,
     allowedHosts: ['localhost', '127.0.0.1', '.loca.lt'],
